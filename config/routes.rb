4c1bdfeb76e05dd55b9ba0f4dd72d147fafdc73a
Francis Servignat <francis.servignat@outlook.com>
2022-10-29 17:13:37 +0200

    fix routes bars, removed users

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: "bars#home"
-  resources :users, only: [:new, :create] do
-    resources :bars
+  resources :bars do
+    resources :bookings, only: [:new, :create]
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
