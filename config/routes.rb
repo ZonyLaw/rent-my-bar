fa4cca5dbd248856aadbcae95814e3434b9faf45
Francis Servignat <francis.servignat@outlook.com>
2022-10-29 17:10:08 +0200

    fix routes added do end

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   root to: "bars#home"
-  resources :users, only: [:new, :create]
+  resources :users, only: [:new, :create] do
     resources :bars
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
