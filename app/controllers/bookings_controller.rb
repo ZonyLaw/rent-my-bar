7f2f28073deb28c0631b007a7b84449ffdaffc5b
Sunny Law <sunny_law@hotmail.com>
2022-11-03 19:00:46 +0000

    add controller to booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,4 +1,5 @@
 class BookingsController < ApplicationController
+  before_action :set_bar, only: [:new, :create]
 
   def new
     @booking = Booking.new
@@ -6,10 +7,20 @@ class BookingsController < ApplicationController
 
   def create
     @booking = Booking.new (booking_params)
-    @booking.save!
+    @booking.bar = @bar
+
+    if @booking.save
+      redirect_to bar_path(@bar)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   private
+
+  def set_bar
+    @bar = Bar.find(params[:bar_id])
+  end
 
   def booking_params
     params.require(:booking).permit(:start_date, :end_date)
