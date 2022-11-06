6c38af85b0630a484898bfa5c4f21db3f0785f5d
Francis Servignat <francis.servignat@outlook.com>
2022-11-06 13:09:52 +0100

    commit conflicts resolved

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema[7.0].define(version: 2022_11_05_125121) do
-=======
-ActiveRecord::Schema[7.0].define(version: 2022_11_03_191601) do
->>>>>>> master
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
