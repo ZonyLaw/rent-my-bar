555872aee0ec1a223ad13a779c99ea1e20af1986
Sunny Law <sunny_law@hotmail.com>
2022-11-12 11:26:58 +0000

    move appliation file to asset folder

--- app/javascript/application.js
+++
@@ -1,9 +0,0 @@
-// Entry point for the build script in your package.json
-import "@hotwired/turbo-rails"
-import "./controllers"
-import "bootstrap"
-
-import { application } from "./controllers/application.js"
-
-import NavbarController from "./controllers/navbar_controller.js"
-application.register("navbar", NavbarController)
